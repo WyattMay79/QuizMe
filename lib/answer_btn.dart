7a71dcb54a0d0fff723406029eedec44a34c7ce9
Wyatt May <wyattmay79@gmail.com>
2024-07-14 15:53:18 -0600

    Added google_fonts pkg from pub.dev, updated the pubspec.yaml file for the changes, updated some of the outdated dependencies, and styled some of the Widgets with the fonts for a better look. I also refactored the the btns and other stuff.

--- lib/answer_btn.dart
+++ lib/answer_btn.dart
@@ -25,7 +25,10 @@ class AnswerBtn extends StatelessWidget {
           borderRadius: BorderRadius.circular(40),
         ),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
